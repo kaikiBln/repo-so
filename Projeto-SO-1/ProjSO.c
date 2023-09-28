ffccd18c9887054e7a2bc9b14e3e39abb8a3ebf0
Kaiki Bellini <109828068+kaikiBln@users.noreply.github.com>
2023-09-28 17:10:53 -0300

    Refatoracao limpeza e ajustes nas threads

--- Projeto-SO-1/ProjSO.c
+++ Projeto-SO-1/ProjSO.c
@@ -2,25 +2,27 @@
 #include <pthread.h>
 #include <sched.h>
 #include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
-#define FIBER_STACK 1024*64
+#define STACK_SIZE 1024 * 64
 
-struct c {
-    int saldo;
-};
-typedef struct c conta;
-
-conta from, to;
-int valor;
-pthread_mutex_t mtx;
 static int flag = 0;
 
-void* transferencia(void* arg) {
+typedef struct {
+    int saldo;
+} Conta;
+
+Conta from, to;
+int valor;
+pthread_mutex_t mtx;
+
+void* realizarTransferencia(void* param) {
     pthread_mutex_lock(&mtx);
 
     if (from.saldo >= valor && flag == 0) {
@@ -49,26 +51,39 @@ void* transferencia(void* arg) {
 }
 
 int main() {
-    pthread_t threads[10];
+    void* stackMemory;
+    pthread_t threadArray[100];
+
+    stackMemory = malloc(STACK_SIZE);
+    if (!stackMemory) {
+        fprintf(stderr, "Erro ao alocar memória para a pilha.\n");
+        return EXIT_FAILURE;
+    }
+
     pthread_mutex_init(&mtx, NULL);
+    from.saldo = 300;
+    to.saldo = 0;
 
-    from.saldo = 100;
-    to.saldo = 100;
-    printf("Transferindo 10 para a conta c2\n");
-    valor = 10;
+    printf("Iniciando transferência de $1 para a conta c2.\n");
+    valor = 50;
 
-    for (int i = 0; i < 10; i++) {
-        if (pthread_create(&threads[i], NULL, transferencia, NULL)) {
-            perror("pthread_create");
-            exit(2);
+    for (int idx = 0; idx < 100; idx++) {
+        if (pthread_create(&threadArray[idx], NULL, realizarTransferencia, NULL)) {
+            fprintf(stderr, "Erro ao criar thread.\n");
+            return EXIT_FAILURE;
         }
     }
 
-    for (int i = 0; i < 10; i++) {
-        pthread_join(threads[i], NULL);
+    for (int idx = 0; idx < 100; idx++) {
+        if (pthread_join(threadArray[idx], NULL)) {
+            fprintf(stderr, "Erro ao juntar thread.\n");
+            return EXIT_FAILURE;
+        }
     }
 
     pthread_mutex_destroy(&mtx);
-    printf("Transferências concluídas e memória liberada.\n");
-    return 0;
+    free(stackMemory);
+
+    printf("Todas as transferências foram realizadas e a memória foi liberada.\n");
+    return EXIT_SUCCESS;
 }
